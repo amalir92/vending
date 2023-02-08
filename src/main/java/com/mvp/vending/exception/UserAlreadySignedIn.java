72e331d1110aa0c27fdad378492db08bdddfd74c
amalir92 <75950528+amalir92@users.noreply.github.com>
2023-02-08 10:38:26 +0530

    error message fix

--- src/main/java/com/mvp/vending/exception/UserAlreadySignedIn.java
+++ src/main/java/com/mvp/vending/exception/UserAlreadySignedIn.java
@@ -5,7 +5,7 @@ public class UserAlreadySignedIn extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     public UserAlreadySignedIn() {
-        super("User user already logged in. Please close other sessions to proceed");
+        super("There is already an active session using your account!");
     }
 
 }
